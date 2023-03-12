1977fe1fd306c88678ddaf34cd0435c5076a6ad0
ldfracao <ldfracao@protonmail.com>
2023-03-12 17:22:52 -0300

    add logic to handle insertion of the form data into a table in the database

--- exseq.js
+++ exseq.js
@@ -23,19 +23,40 @@ app.get("/", (req, res) => {
 // handles form post request
 app.post("/", (req, res) => {
   const name = req.body.name;
-  console.log("Nome", name);
+  const tel = req.body.tel;
+  const origin = req.body.origin;
+  const date = req.body.date;
+  const obs = req.body.obs;
 
+  // define model for Agendamentos table
   const Agendamento = sequelize.define('Agendamento', {
     nome: {
       type: DataTypes.STRING
+    },
+    telefone: {
+      type: DataTypes.STRING
+    },
+    origem: {
+      type: DataTypes.STRING
+    },
+    data: {
+      type: DataTypes.DATE
+    },
+    obs: {
+      type: DataTypes.STRING
     }
-
   });
-  Agendamento.sync().then(
-    function (nome){
-      Agendamento.create({ nome: name });
-    });
 
+  // .sync() creates a table if it doesn't exists, else does nothing
+  Agendamento.sync().then(
+    function (nome, telefone, origem, data, obs){
+      Agendamento.create(
+        { nome: name },
+        { telefone: tel },
+        { origem: origin },
+        { data: date },
+        { obs: obs });
+    });
   res.sendFile(__dirname + "/index.html");
 })
 
